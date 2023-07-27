880e0419a95bb4bc095648aa46bb7156aaa80c95
Duy Pham <duypham@DuyPham-MO.local>
2023-07-27 11:47:24 +0700

    init

---
+++ swiftios/swiftiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftiosApp.swift
+//  swiftios
+//
+//  Created by Duy Pham on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
